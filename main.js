0b3955a9ab9d8af5f2eee061166fa3f58506be00
Tiipuuba <142894444+Tiipuuba@users.noreply.github.com>
2025-02-19 10:36:08 +0200

    Profile and message stuff
    Added test of how profiles and messages should work through localstorage.

---
+++ main.js
@@ -0,0 +1,32 @@
+// Käyttäjän ominaisuudet
+let kayttaja = {
+    name: "Pirkko",
+    password: "salasana",
+    messages: {
+    }
+}
+
+// Ostajan "viestin" sisältö
+let ostaja = "Vesa"
+let tarjous = "2e"
+
+// Viesti lisätään myyjän profiiliin
+kayttaja.messages[ostaja] = [tarjous]
+
+//Muutetaan oikeaan muotoon ja tallennetaan localstorageen
+localStorage.setItem('Pirkko', JSON.stringify(kayttaja))
+
+// Puretaan ja tuodaan käyttäjä localstoragesta
+var parsittu = JSON.parse(localStorage.getItem('Pirkko'))
+
+// Näillä voi hakea tiedot ja käydä viestit läpi
+// esim kun näytetään viestit profiilissa
+console.log(parsittu.name, parsittu.password);
+
+
+for (let message in parsittu.messages) {
+    console.log(message + ": " + parsittu.messages[message]);
+}
+
+// Poistaa viestin
+delete parsittu.messages['Vesa']
