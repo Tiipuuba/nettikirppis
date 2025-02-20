d876732e9124b5b3a0a69cadf2e3828493a97f78
Tiipuuba <142894444+Tiipuuba@users.noreply.github.com>
2025-02-20 14:00:33 +0200

    Added modal for making a listing
    Modal takes all of the information needed for making a listing.

--- main.js
+++ main.js
@@ -30,3 +30,10 @@ for (let message in parsittu.messages) {
 
 // Poistaa viestin
 delete parsittu.messages['Vesa']
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
